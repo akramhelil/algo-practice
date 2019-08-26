f800aee24c07a46de7e7f883547ac03009ad0f8d
akramhelil <akramhelil@gmail.com>
2019-08-26 17:02:22 -0400

    paldirome

--- algo-practice.js
+++ algo-practice.js
@@ -1,12 +1,28 @@
 // reverse a string
 function reverse(str) {
-    let reversed = "";
-    for (let char of str) {
-        reversed = char + reversed;
-    }
-    return console.log(reversed);
+    // solution one 
+    // let reversed = "";
+    // for (let char of str) {
+    //     reversed = char + reversed;
+    // }
+    // return console.log(reversed);
     
+    // solution two
+    const reversed =  str.split('')
+        .reverse()
+        .join('')
+    return console.log(reversed);
 }
+
+
+function palindrome(str) {
+    //  my solution 
+    let reversed = '';
+    for (let char of str) {
+        reversed = char + reversed
+    }
+    return reversed === str ? true : false;
+    }
 
 reverse('Akram Helil')
 
