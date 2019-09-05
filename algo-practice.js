88d981ec87f6b5872e7ffa1a287a3d2bf531bce5
akramhelil <akramhelil@gmail.com>
2019-09-05 12:08:52 -0400

    fizbuzz

--- algo-practice.js
+++ algo-practice.js
@@ -95,6 +95,23 @@ function maxChar(str) {
 
 }
 
+// FIZBUZZZZZ 
+function fizzBuzz(num) {
+    // my solution  modulo operator % 
+    for (let i = 1; i <= n; i++){
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+        } else if (i % 3 === 0) {
+            console.log('fizz')
+        } else if (i % 5 === 0) {
+            console.log('buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+
 
 
 
