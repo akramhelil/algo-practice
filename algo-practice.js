f7117e0254a219809ca997c37c7c2f65e361717e
akramhelil <akramhelil@gmail.com>
2019-09-17 16:02:58 -0400

    string captilization

--- algo-practice.js
+++ algo-practice.js
@@ -216,7 +216,13 @@ function cleanString(str) {
 
 /* --------------------------------------------------------*/
 
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
 
+function capitalize(str) {
+    const words = []
+}
 
 
 
