99cc4b2d51df077959be1fa097551a7b3a2d7438
akramhelil <akramhelil@gmail.com>
2020-01-21 15:04:46 -0500

    vowvs check

--- algo-practice.js
+++ algo-practice.js
@@ -358,7 +358,6 @@ function pyramid(n, row = 0, level = '') {
 }
 
 
-module.exports = pyramid;
 
 
 
@@ -383,7 +382,26 @@ module.exports = pyramid;
 // }
 
 
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
 
+function vowels(str) {
+    let count = 0;
+    const checkerStr = ['a','e','i','o','u'];
+
+    for (let char of str.toLowerCase()) {
+        if (checkerStr.includes(char)) {
+            count++;
+        }
+    }
+    
+    return count;
+}
 
 
 
